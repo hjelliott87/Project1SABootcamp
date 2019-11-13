e3d03e895274b4b12ae0a67e91ad968e93e2e4d2
qsccsafety <germex2006@gmail.com>
2019-11-12 18:40:01 -0600

    added searchbar
    added search bar to html

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
+var settings = {
+  async: true,
+  crossDomain: true,
+  url:
+    "https://cors-anywhere.herokuapp.com/https://travel-advisory.p.rapidapi.com/api",
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "travel-advisory.p.rapidapi.com",
+    "x-rapidapi-key": "8ad2e98342msh27fc3fc5b062d30p1d80a5jsn25f0073e8a8c"
+  }
+};
 
-      var settings = {
-        async: true,
-        crossDomain: true,
-        url:
-          "https://cors-anywhere.herokuapp.com/https://travel-advisory.p.rapidapi.com/api",
-        method: "GET",
-        headers: {
-          "x-rapidapi-host": "travel-advisory.p.rapidapi.com",
-          "x-rapidapi-key": "8ad2e98342msh27fc3fc5b062d30p1d80a5jsn25f0073e8a8c"
-        }
-      };
+$.ajax(settings).done(function(response) {
+  console.log(response);
+});
 
-      $.ajax(settings).done(function(response) {
-        console.log(response)111111111;
-      });
-    
+
+async src="https://cse.google.com/cse.js?cx=001325578064499497546:as7yfkjtdgn"
+
